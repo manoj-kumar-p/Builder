1350f3e626b3aff75c0ff8a6c8c6b3a07adbb28f
as766 <as7668105@gmail.com>
2020-09-28 15:24:13 +0530

    initial

--- app/src/main/java/com/solutions/app/MainActivity.java
+++ app/src/main/java/com/solutions/app/MainActivity.java
@@ -3,11 +3,11 @@ package com.solutions.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
